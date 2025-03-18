057a005d8b999c561edb90090b1c4885f2e58c52
Daiane Machado <dai.mmachado@gmail.com>
2025-03-18 03:25:48 -0300

    fix

--- src/main/java/com/example/exercicios_java/ExerciciosJavaApplication.java
+++ src/main/java/com/example/exercicios_java/ExerciciosJavaApplication.java
@@ -5,10 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.example.exercicios_java.semana1.negocio","com.example.exercicios_java.semana1.service","com.example.exercicios_java.semana1.controlle"})
+@ComponentScan(basePackages = {"com.example.exercicios_java.semana1.negocio","com.example.exercicios_java.semana1.service","com.example.exercicios_java.semana1.controller"})
 public class ExerciciosJavaApplication {
 
-	public static void main(String[] args) {r
+	public static void main(String[] args) {
+		
 		SpringApplication.run(ExerciciosJavaApplication.class, args);
 	}
 
