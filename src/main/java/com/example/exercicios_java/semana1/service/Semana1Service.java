9cd7daefb3719d2fd318542056a18f4b746259bd
Daiane Machado <dai.mmachado@gmail.com>
2025-03-18 02:59:40 -0300

    fix

--- src/main/java/com/example/exercicios_java/semana1/service/Semana1Service.java
+++ src/main/java/com/example/exercicios_java/semana1/service/Semana1Service.java
@@ -1,18 +1,15 @@
 package com.example.exercicios_java.semana1.service;
 
-import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 
 @Service
 public class Semana1Service{
 
-    private final Ex1Negocio ex1Negocio;
-
     @Autowired
-    public Semana1Service(Ex1Negocio ex1Negocio){
-        this.ex1Negocio = ex1Negocio;
-    }
+    private Ex1Negocio ex1Negocio;
+
     public String ex1Hello(){
         return ex1Negocio.imprimeHello();
     }
