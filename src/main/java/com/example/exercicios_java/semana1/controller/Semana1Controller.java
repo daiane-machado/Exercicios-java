cf50279b275ddd65e5abe3352ccbc2fd961cdb79
Daiane Machado <dai.mmachado@gmail.com>
2025-03-18 02:24:35 -0300

    fix

--- src/main/java/com/example/exercicios_java/semana1/controller/Semana1Controller.java
+++ src/main/java/com/example/exercicios_java/semana1/controller/Semana1Controller.java
@@ -3,16 +3,14 @@ package com.example.exercicios_java.semana1.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @RestController
 @RequestMapping(value = "/semana1")
 public class Semana1Controller {
 
+    @Autowired
     private final Semana1Service semana1Service;
-
-    public Semana1Controller(Semana1Service semana1Service){
-        this.semana1Service = semana1Service;
-    }
     
     @GetMapping("/ex1")
     public String ex1()  {
