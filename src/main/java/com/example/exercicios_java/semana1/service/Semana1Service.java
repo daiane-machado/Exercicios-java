702bbefbe8a5af3044028b202bb40f81610d9959
Daiane Machado <dai.mmachado@gmail.com>
2025-03-18 03:33:28 -0300

    teste

--- src/main/java/com/example/exercicios_java/semana1/service/Semana1Service.java
+++ src/main/java/com/example/exercicios_java/semana1/service/Semana1Service.java
@@ -9,11 +9,9 @@ import com.example.exercicios_java.semana1.negocio.Ex1Negocio;
 @Service
 public class Semana1Service{
 
-   
-    private final Ex1Negocio ex1Negocio;
-public Semana1Service(Ex1Negocio ex1Negocio){
-    this.ex1Negocio = ex1Negocio;
-}
+   @Autowired
+    private Ex1Negocio ex1Negocio;
+
     public String ex1Hello(){
         return ex1Negocio.imprimeHello();
     }
