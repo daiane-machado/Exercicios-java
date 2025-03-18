9747d22baafb0abdc8edce094efafe6139d8927d
Daiane Machado <dai.mmachado@gmail.com>
2025-03-18 03:18:41 -0300

    fix

--- src/main/java/com/example/exercicios_java/ExerciciosJavaApplication.java
+++ src/main/java/com/example/exercicios_java/ExerciciosJavaApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.example.exercicios_java.semana1.negocio","com.example.exercicios_java.semana1.services","com.example.exercicios_java.semana1.controlle"})
 public class ExerciciosJavaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ExerciciosJavaApplication.class, args);
+		SpringApplication.run(ExerciciosJavaApplication.class, args);r
 	}
 
 }
